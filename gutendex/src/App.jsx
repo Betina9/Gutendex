aa855801f96d42157c1567cb905ba46bfbccca13
Betina9 <ob042025@gmail.com>
2025-09-04 11:20:26 +0200

    Refactor App component to include Header and Footer, and update CSS styles

--- gutendex/src/App.jsx
+++ gutendex/src/App.jsx
@@ -1,10 +1,12 @@
+import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <div>
-      <h1>App fra router</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
