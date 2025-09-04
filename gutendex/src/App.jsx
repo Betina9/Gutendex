c409e9b77cd064e46ca8843f64c15c31677538dd
Betina9 <ob042025@gmail.com>
2025-09-04 10:50:01 +0200

    Refactor project structure and add routing
    - Updated index.html to correct DOCTYPE and title.
    - Added react-router-dom dependency for routing.
    - Modified package.json and package-lock.json to include new dependencies.
    - Refactored App component to use RouterProvider for routing.
    - Created About component for the About page.

--- gutendex/src/App.jsx
+++ gutendex/src/App.jsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+export default function App() {
+  return (
+    <div>
+      <h1>App fra router</h1>
+    </div>
+  );
 }
-
-export default App;
--- gutendex/src/main.jsx
+++ gutendex/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+//Router components
+import App from "./App.jsx";
+import "./About.jsx";
+import About from "./About.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <p>Error</p>,
+  },
+  {
+    path: "*",
+    element: <p>404 - Not Found</p>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
